db0ef95dfe75aefc17eecaa7c8c5484d82db61dd
Maligna.sospechosa <lulikunka@gmail.com>
2024-09-18 08:11:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let link=document.querySelector('a');
+
+link.addEventListener('click',e => {
+    e.preventDefault();
+    aviso();
+});
+let aviso= ()=>{
+    alert('Mueva el mouse en de izquierda a derecha y de arriba hacia abajo para modificar la obra');
+    location.href="https://lulikunka.github.io/arte-generativo/";
+}
